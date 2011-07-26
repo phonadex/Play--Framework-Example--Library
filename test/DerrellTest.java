e40729afa57f779c0a6e6f318813779c441bc212
phonadex <phonadex@yahoo.com>
2011-07-26 12:10:39 -0500

    Fixes #2.  Missing statement

--- test/DerrellTest.java
+++ test/DerrellTest.java
@@ -9,6 +9,7 @@ public class DerrellTest extends UnitTest {
     @Test
     public void constructorTest() {
 	Book b = new Book("Title", "Author");
+	assertNotNull(b);
 	assertFalse(b.isCheckedOut);
 	b.checkOut();
 	assertTrue(b.isCheckedOut);
