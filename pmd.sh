b7c642c0d60abd3b6aad22f329ea4209a07782dc
phonadex <phonadex@yahoo.com>
2011-07-26 15:54:35 -0500

    changed path for findbugs and pmd

--- pmd.sh
+++ pmd.sh
@@ -1,1 +1,1 @@
-~/third/pmd-4.2.5/bin/pmd.sh app xml basic,imports,unusedcode,naming,braces,design,strings > test-result/pmd.xml
+/opt/pmd/bin/pmd.sh app xml basic,imports,unusedcode,naming,braces,design,strings > test-result/pmd.xml
